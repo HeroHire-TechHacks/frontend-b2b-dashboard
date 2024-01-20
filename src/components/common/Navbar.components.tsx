eb515a7c7be2fc7f8c6a8cf2c7260b752ca9c87f
Aditya Krishna Gupta <aditya.kg169@gmail.com>
2024-01-21 03:20:34 +0530

    feat: add logout

--- src/components/common/Navbar.components.tsx
+++ src/components/common/Navbar.components.tsx
@@ -1,8 +1,17 @@
+import { useNavigate } from 'react-router-dom';
+import { Button } from '../ui/button';
+
 const Navbar = ({ page }: { page?: string }) => {
+	const navigate = useNavigate();
+
 	return (
-		<nav className="border border-eaeaea w-full">
+		<nav className="border border-eaeaea w-full flex flex-row justify-between items-center">
 			<h1 className="p-4 text-xl font-inter font-medium ml-5">HeroHire</h1>
-			{/* {page === 'login' || page === 'error' ? null : <Button>Logout</Button>} */}
+			{page === 'login' || page === 'error' ? null : (
+				<Button className="mr-5" onClick={() => navigate('/login')}>
+					Logout
+				</Button>
+			)}
 		</nav>
 	);
 };
